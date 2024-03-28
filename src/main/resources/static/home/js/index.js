5f65d8bd236ad15549a9570c8061d15329e4fb89
Nolde7w7 <137129221+noldedev@users.noreply.github.com>
2024-03-27 20:07:40 -0500

    Index Pls

--- src/main/resources/static/home/js/index.js
+++ src/main/resources/static/home/js/index.js
@@ -1,3 +1,31 @@
+/*------------------------------------------------------------INICIO HEADER---------------------------------------------------- */
+document.addEventListener("DOMContentLoaded", function () {
+  var label_open_silebar = document.querySelector("label[for='open-silebar']");
+  var label_close_silebar = document.querySelector(
+    "label[for='close-silebar']"
+  );
+
+  var contenedor_silebar = document.getElementById("background-silebar");
+  var silevar = document.getElementById("contenedor-silebar");
+
+  label_open_silebar.addEventListener("click", function () {
+    silevar.style.display = "block"; // Mostrar el sidebar
+    setTimeout(() => {
+      silevar.style.left = "0"; // Mover a la posición inicial
+    }, 50); // Pequeña espera antes de mover para permitir que se muestre el sidebar primero
+    contenedor_silebar.style.display = "block"; // Mostrar el fondo del sidebar
+  });
+
+  label_close_silebar.addEventListener("click", function () {
+    silevar.style.left = "-300px"; // Mover a la izquierda
+    setTimeout(() => {
+      silevar.style.display = "none"; // Ocultar después de la animación
+      contenedor_silebar.style.display = "none"; // Ocultar el fondo del sidebar
+    }, 300); // Esperar la duración de la animación (0.3s)
+  });
+});
+/*------------------------------------------------------------FIN HEADER---------------------------------------------------- */
+
 /*------------------------------------------------------------SWIPER CODE---------------------------------------------------- */
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 4,
