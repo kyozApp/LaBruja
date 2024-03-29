3cfd3957d38dbfb8ed8138c220e9e955f63bb864
kyozApp <151455520+kyozApp@users.noreply.github.com>
2024-03-29 00:19:11 -0500

    Modificacion de los banners y marcas agregando tabla

--- src/main/java/com/labrujastore/controller/HomeController.java
+++ src/main/java/com/labrujastore/controller/HomeController.java
@@ -1,14 +1,34 @@
 package com.labrujastore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.labrujastore.entity.Banner;
+import com.labrujastore.entity.Marca;
+import com.labrujastore.service.BannerService;
+import com.labrujastore.service.MarcaService;
+
 @Controller
-public class HomeController 
-{
+public class HomeController {
+	@Autowired
+	private BannerService bannerService;
+	@Autowired
+	private MarcaService marcaService;
+
 	@GetMapping("/")
-	public String index()
-	{
+	public String index(Model model) {
+		List<Marca> marcas = marcaService.listarMarca();
+		Banner bannerId1 = bannerService.obtenerIdBanner(1);
+		Banner bannerId2 = bannerService.obtenerIdBanner(2);
+		Banner bannerId3 = bannerService.obtenerIdBanner(3);
+		model.addAttribute("vistaMarcas", marcas);
+		model.addAttribute("bannerId1", bannerId1);
+		model.addAttribute("bannerId2", bannerId2);
+		model.addAttribute("bannerId3", bannerId3);
 		return "index";
 	}
 }
