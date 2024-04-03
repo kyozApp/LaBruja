504ae2a79245a2037f1990ffd683ebfe2f72c3f0
DevurCode <aracaalvarezd@gmail.com>
2024-04-03 16:49:16 -0500

    correcciones del footer y header

--- src/main/java/com/labrujastore/controller/ContactanosController.java
+++ src/main/java/com/labrujastore/controller/ContactanosController.java
@@ -1,7 +1,15 @@
 package com.labrujastore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.labrujastore.entity.Categoria;
+import com.labrujastore.service.CategoriaService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,8 +17,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/contactanos")
 public class ContactanosController {
 
+    @Autowired
+    private CategoriaService categoriaService;
+
     @GetMapping("")
-    public String index_GET() {
+    public String index_GET(Model model) {
+
+        List<Categoria> categorias = categoriaService.listarCategoria();
+        model.addAttribute("vistaCategorias", categorias);
+
         return "contactanos/index";
     }
     
