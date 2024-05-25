6d7a11898cd26928dba0bae27df2ef737a10027f
Devur <aracaalvarezd@gmail.com>
2024-05-25 10:38:34 -0500

    creacionn del apartado de about

---
+++ src/main/java/com/labrujastore/controller/AboutController.java
@@ -0,0 +1,115 @@
+package com.labrujastore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.labrujastore.entity.Accesorio;
+import com.labrujastore.entity.Almacenamiento;
+import com.labrujastore.entity.Casse;
+import com.labrujastore.entity.Categoria;
+import com.labrujastore.entity.Combo;
+import com.labrujastore.entity.Fuente;
+import com.labrujastore.entity.Laptop;
+import com.labrujastore.entity.Monitor;
+import com.labrujastore.entity.Placa;
+import com.labrujastore.entity.Procesador;
+import com.labrujastore.entity.Ram;
+import com.labrujastore.entity.Refrigeracion;
+import com.labrujastore.entity.Tarjeta;
+import com.labrujastore.service.AccesorioService;
+import com.labrujastore.service.AlmacenamientoService;
+import com.labrujastore.service.CasseService;
+import com.labrujastore.service.CategoriaService;
+import com.labrujastore.service.ComboService;
+import com.labrujastore.service.FuenteService;
+import com.labrujastore.service.LaptopService;
+import com.labrujastore.service.MonitorService;
+import com.labrujastore.service.PlacaService;
+import com.labrujastore.service.ProcesadorService;
+import com.labrujastore.service.RamService;
+import com.labrujastore.service.RefrigeracionService;
+import com.labrujastore.service.TarjetaService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/about")
+public class AboutController {
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @Autowired
+    private AccesorioService accesorioService;
+
+    @Autowired
+    private AlmacenamientoService almacenamientoService;
+
+    @Autowired
+    private CasseService casseService;
+
+    @Autowired
+    private FuenteService fuenteService;
+
+    @Autowired
+    private LaptopService laptopService;
+
+    @Autowired
+    private MonitorService monitorService;
+
+    @Autowired
+    private PlacaService placaService;
+
+    @Autowired
+    private ProcesadorService procesadorService;
+
+    @Autowired
+    private RamService ramService;
+
+    @Autowired
+    private RefrigeracionService refrigeracionService;
+
+    @Autowired
+    private TarjetaService tarjetaService;
+
+    @Autowired
+    private ComboService comboService;
+
+    @GetMapping("")
+    public String index_GET(Model model) {
+
+        List<Categoria> categorias = categoriaService.listarCategoria();
+        model.addAttribute("vistaCategorias", categorias);
+
+        List<Accesorio> accesorios = accesorioService.listarAccesorio();
+        List<Almacenamiento> almacenamientos = almacenamientoService.listarAlmacenamiento();
+        List<Casse> casses = casseService.listarCasse();
+        List<Fuente> fuentes = fuenteService.listarFuente();
+        List<Laptop> laptops = laptopService.listarLaptop();
+        List<Monitor> monitores = monitorService.listarMonitor();
+        List<Placa> placas = placaService.listarPlaca();
+        List<Procesador> procesadores = procesadorService.listarProcesador();
+        List<Ram> rams = ramService.listarRam();
+        List<Refrigeracion> refrigeraciones = refrigeracionService.listarRefrigeracion();
+        List<Tarjeta> tarjetas = tarjetaService.listarTarjeta();
+        List<Combo> combos = comboService.listarCombo();
+
+        model.addAttribute("vistaAccesorios", accesorios);
+        model.addAttribute("vistaAlmacenamientos", almacenamientos);
+        model.addAttribute("vistaCasses", casses);
+        model.addAttribute("vistaFuentes", fuentes);
+        model.addAttribute("vistaLaptops", laptops);
+        model.addAttribute("vistaMonitores", monitores);
+        model.addAttribute("vistaPlacas", placas);
+        model.addAttribute("vistaProcesadores", procesadores);
+        model.addAttribute("vistaRams", rams);
+        model.addAttribute("vistaRefrigeraciones", refrigeraciones);
+        model.addAttribute("vistaTarjetas", tarjetas);
+        model.addAttribute("vistaCombos", combos);
+
+        return "about/index";
+    }
+}
