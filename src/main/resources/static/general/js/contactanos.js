b50b9a3952f941eaf46c8cac298ea9d5b4bae78c
Nolde7w7 <137129221+noldedev@users.noreply.github.com>
2024-05-27 12:31:04 -0500

    cotizaciones

--- src/main/resources/static/general/js/contactanos.js
+++ src/main/resources/static/general/js/contactanos.js
@@ -1,4 +1,15 @@
+// Función para eliminar los productos del carrito del localStorage
+function limpiarProductosEnCarrito() {
+  localStorage.removeItem("productosEnCarrito");
+}
+// Función para eliminar la cotización del localStorage
+function limpiarCotizacion() {
+  localStorage.removeItem("cotizacion");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+  actualizarCantidadCarrito();
+
   var enlacesWhatsApp = document.querySelectorAll("a[href*='wa.me']");
   enlacesWhatsApp.forEach(function (enlace) {
     enlace.addEventListener("click", function (event) {
@@ -20,17 +31,32 @@ document.addEventListener("DOMContentLoaded", function () {
         saludo = "Buenas noches";
       }
 
-      // Construir el mensaje con los productos del carrito
-      var mensaje = `¡${saludo}, deseo comprar estos productos!\n\nProductos:\n`;
+      // Construir el mensaje con los productos del carrito o la cotización
+      var mensaje;
+      var cotizacion = localStorage.getItem("cotizacion");
+      if (cotizacion) {
+        mensaje = `¡${saludo}, deseo cotizar estos productos! Productos\n\n${cotizacion}`;
 
-      // Obtener los productos del carrito del localStorage
-      var productosEnCarrito =
-        JSON.parse(localStorage.getItem("productosEnCarrito")) || [];
+        // Eliminar la cotización del localStorage
+        limpiarCotizacion();
+      } else {
+        mensaje = `¡${saludo}, deseo comprar estos productos!\n\nProductos:\n`;
 
-      // Recorrer los productos y agregarlos al mensaje con la cantidad
-      productosEnCarrito.forEach(function (producto) {
-        mensaje += `${producto.nombre} - ${producto.precio}  x${producto.cantidad}\n`;
-      });
+        // Obtener los productos del carrito del localStorage
+        var productosEnCarrito =
+          JSON.parse(localStorage.getItem("productosEnCarrito")) || [];
+
+        // Recorrer los productos y agregarlos al mensaje con la cantidad
+        productosEnCarrito.forEach(function (producto) {
+          mensaje += `${producto.nombre} - ${producto.precio}  x${producto.cantidad}\n`;
+        });
+
+        // Eliminar los productos del carrito del localStorage
+        limpiarProductosEnCarrito();
+
+        // Actualizar la cantidad en el contador del carrito
+        actualizarCantidadCarrito();
+      }
 
       // Codificar el mensaje completo
       mensaje = encodeURIComponent(mensaje);
@@ -43,3 +69,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Función para actualizar la cantidad en el contador del carrito
+function actualizarCantidadCarrito() {
+  var productosEnCarrito =
+    JSON.parse(localStorage.getItem("productosEnCarrito")) || [];
+  var cantidad = productosEnCarrito.reduce(function (total, producto) {
+    return total + producto.cantidad;
+  }, 0);
+  document.getElementById("cantidad-contador-carrito").textContent = cantidad;
+}
